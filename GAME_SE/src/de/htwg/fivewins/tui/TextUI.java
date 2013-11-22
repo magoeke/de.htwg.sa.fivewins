a295e8ec277844f99d640570823df7591b1e0b01
magoeke <magoeke@3DLAB9.in.fhkn.de>
2013-11-22 12:18:43 +0100

    Changes for checkstyle

--- GAME_SE/src/de/htwg/fivewins/tui/TextUI.java
+++ GAME_SE/src/de/htwg/fivewins/tui/TextUI.java
@@ -5,14 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import de.htwg.fivewins.controller.FiveWinsController;
+import de.htwg.fivewins.field.Field;
 import de.htwg.util.observer.IObserver;
 
 public class TextUI implements IObserver{
 
 	
 	private FiveWinsController controller;
-	Scanner scanner;
-	String line = "";
+	private Scanner scanner;
+	//private String line = "";
 	
 
 	public TextUI(FiveWinsController controller){
@@ -31,9 +32,9 @@ public class TextUI implements IObserver{
 	}
 
 	public void printTUI() {
-		System.out.println(controller.getFieldString());
-		System.out.println(controller.getStatus());
-		System.out.println("Please enter a command( q - quit, u - update, s - solve, r - reset, n - new, 1 - 50 - set size, x y - show candidates at (x,y), xyz - set cell(x,y) to z):");
+		System.out.print(controller.getFieldString() + "\n");
+		System.out.print(controller.getStatus() + "\n");
+		System.out.print("Please enter a command( q - quit, u - update, n - new, 1 - 15 - set size, x y - set cell(x,y)):\n");
 	}
 	
 	public boolean handleInputOrQuit(String line) {
@@ -44,9 +45,12 @@ public class TextUI implements IObserver{
 		if (line.equalsIgnoreCase("u")) {
 			//Do nothing, just redraw the updated grid
 		}
-
-		// if the command line has the form 123, set the cell (1,2) to value 3
-		if (line.matches("[0-50][0-50]")){
+		if (line.equalsIgnoreCase("n")) {
+			//Restart game
+			new TextUI(new FiveWinsController(new Field(3)));
+		}
+		// if the command line has the form 12, set the cell (1,2) to value 3
+		if (line.matches("[0-9]{1,2} [0-9]{1,2}")){
 			Pattern p = Pattern.compile("[0-50]");
 			Matcher m = p.matcher(line);
 			int[] arg = new int[2];
