47469f80862665147480aa7c411c8ded4013bfb2
magoeke <magoeke@htwg-konstanz.de>
2015-04-01 21:22:56 +0200

    Added IFieldFactory.

---
+++ GAME_SE/src/main/java/de/htwg/fivewins/model/field/IFieldFactory.java
@@ -0,0 +1,12 @@
+package de.htwg.fivewins.model.field;
+
+public interface IFieldFactory {
+
+		/**
+		 * Creates an IField Object.
+		 * @param fieldesize
+		 * @return
+		 */
+		IField create(int fieldesize);
+	
+}
