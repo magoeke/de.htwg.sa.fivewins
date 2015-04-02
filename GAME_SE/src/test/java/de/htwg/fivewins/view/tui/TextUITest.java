07d42ea4e6305aa202ab13c20579e04347ff3da8
magoeke <magoeke@htwg-konstanz.de>
2015-04-02 11:28:27 +0200

    Added one testcase to tui

--- GAME_SE/src/test/java/de/htwg/fivewins/view/tui/TextUITest.java
+++ GAME_SE/src/test/java/de/htwg/fivewins/view/tui/TextUITest.java
@@ -1,5 +1,29 @@
 package de.htwg.fivewins.view.tui;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.htwg.fivewins.controller.FiveWinsController;
+import de.htwg.fivewins.controller.IFiveWinsController;
+import de.htwg.fivewins.model.field.FieldFactory;
+import de.htwg.fivewins.model.field.IFieldFactory;
+
 public class TextUITest {
 
+	TextUI textUI;
+	
+	@Before
+	public void setUp() {
+		IFieldFactory tmpFieldFactory = new FieldFactory();
+		IFiveWinsController tmpController = new FiveWinsController(tmpFieldFactory);
+		textUI = new TextUI(tmpController);
+	}
+	
+	@Test
+	public void testIterate() {
+		final String testCommand = "1,1";
+		assertFalse(textUI.iterate(testCommand));
+	}
 }
