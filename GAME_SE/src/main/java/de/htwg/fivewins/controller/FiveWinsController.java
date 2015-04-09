c430e5dd1d625d2c0f126cedce115943ff1edf34
magoeke <magoeke@htwg-konstanz.de>
2015-04-09 20:16:47 +0200

    Commented 2 lines

--- GAME_SE/src/main/java/de/htwg/fivewins/controller/FiveWinsController.java
+++ GAME_SE/src/main/java/de/htwg/fivewins/controller/FiveWinsController.java
@@ -346,8 +346,8 @@ public class FiveWinsController extends Observable implements
 	 * handel inputed command reset, update or set value
 	 */
 	public boolean handleInputOrQuit(String line) {
-		db.saveField(this.field);
-		System.out.println("From DB:"+db.getFieldById("hallo"));
+//		db.saveField(this.field);
+//		System.out.println("From DB:"+db.getFieldById("hallo"));
 		boolean quit = false;
 		if (line.equalsIgnoreCase("q")) {
 			quit = true;
