0be4ca791f6b770b73d01616cf1f9ed4f40d4889
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2015-06-27 20:14:16 +0200

    added Result & Worker

---
+++ GAME_SE/src/main/java/de/htwg/fivewins/controller/game/Result.java
@@ -0,0 +1,13 @@
+package de.htwg.fivewins.controller.game;
+
+public class Result {
+	int result;
+	
+	public Result(int result) {
+		this.result = result;
+	}
+	
+	public int getResult() {
+		return this.result;
+	}
+}
